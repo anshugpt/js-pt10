1258f045a66f4cc0a2a5d91f851daf0d09079e0e
anshugpt <138896567+anshugpt@users.noreply.github.com>
2023-11-10 10:35:18 +0530

    added notes

---
+++ app.js
@@ -0,0 +1,27 @@
+//* DOM Events 
+// Events are the action done by keybored or mouse
+// Events are signals that something has occurred(user input/actions)
+
+//* Inline Events 
+// attribute --> onclick=""
+
+// in html -> <button onclick="console.log("button was clicked")">click me</button>
+
+// when we click button it will print on the console
+
+//* onclick Event
+// when we click by mouse it will trriger
+
+// let btn = document.querySelector("button")
+
+// btn.onclick = function () {
+//     alert("button is clicked"); //* when we click it will give alert
+// };
+
+
+//* onmouseenter Event
+// when mouse pointer hover over the range of the element 
+
+// btn.onmouseenter = function (){
+//    console.log("you are in range of element");
+//}
