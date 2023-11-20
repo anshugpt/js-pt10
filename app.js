28e9e2b1693ee73dc110473e2a69547984358cd5
anshugpt <138896567+anshugpt@users.noreply.github.com>
2023-11-20 23:02:00 +0530

    updated all three files

--- app.js
+++ app.js
@@ -25,3 +25,55 @@
 // btn.onmouseenter = function (){
 //    console.log("you are in range of element");
 //}
+
+//* Event Listener 
+// we can't perform multiple task on "onclick method" so we need event listener
+// Syntax -> obj.addEventListener("event", callback);
+// event -> "clicked", "drag", many more...(mdn refrence)
+// callback -> function used as attribute
+
+//* Activity (random color generator)
+
+let btn = document.querySelector(".color-btn");
+
+btn.addEventListener("click", function(){
+    let head = document.querySelector(".head");
+    let randomColor = getRandomColor();
+    head.innerText = randomColor;
+    let div = document.querySelector(".color-box");
+    div.style.backgroundColor = randomColor;
+});
+
+function getRandomColor(){
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let color = `rgb(${red}, ${green}, ${blue})`;
+    return color;
+}
+
+// event listener is also used for elements like para, div 
+
+//* "this" in event
+// 'this' reffers to the object
+// used if we want to add same event listener on multiple element
+
+btn.addEventListener("click", function(){
+    console.dir(this); //* -> show the all button property
+    console.dir(this.innerText); //* -> show inner text of button
+})
+
+// for multiple element
+
+//  let h3 = document.querySelector("h3");
+//  let p = document.querySelector("p");
+
+//  function changeColor(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor = "red";
+//  }
+
+//  btn.addEventListener("click", changeColor);
+//  h3.addEventListener("click", changeColor);
+//  p.addEventListener("click", changeColor);
